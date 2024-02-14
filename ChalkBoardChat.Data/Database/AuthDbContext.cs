23d0f5e73a6525fb5697699895092607be26dc5a
pelicanpubis <norah.binsalamah@hotmail.com>
2024-02-14 21:02:12 +0100

    Databas är lagt till i SQL

--- ChalkBoardChat.Data/Database/AuthDbContext.cs
+++ ChalkBoardChat.Data/Database/AuthDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,13 @@ using System.Threading.Tasks;
 
 namespace ChalkBoardChat.Data.Database
 {
-    internal class AuthDbContext
+    public class AuthDbContext : IdentityDbContext
+
     {
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
     }
 }
